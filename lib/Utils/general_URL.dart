d9f7a360deadf363c1728f2af848bd4fcf12d981
MazenScit <D.mazen@scit.co>
2024-01-02 19:24:13 +0200

    100 commit

---
+++ lib/Utils/general_URL.dart
@@ -0,0 +1,13 @@
+String serverUrl = "https://set-institute.net/api";
+String imageUrl = "https://set-institute.net/storage/";
+String FileUrl = "https://set-institute.net/admin/storage";
+late String apiacceptencevariable;
+
+
+
+
+// String serverUrl = "https://kooora83.000webhostapp.com/api";
+
+
+// https://set-institute.net/admin/storage/files/file_1670487036.png
+
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:get/get.dart';
+import 'package:setacademyapp/auth/log_in.dart';
+import 'package:setacademyapp/screen/sendmessagesystem/controller/uaecontroller.dart';
+import 'package:setacademyapp/screen/welcome.dart';
+import 'logale/locale_Cont.dart';
+import 'logale/logale.dart';
+import 'screen/splash.dart';
+
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    Get.put(MyLocaleController());
+    return GetMaterialApp(
+      translations: MyLocale(),
+      theme: ThemeData(
+        fontFamily: 'Cobe',
+      ),
+      debugShowCheckedModeBanner: false,
+      home: WalkThroughScreen(),
+      routes: {
+        'login': ((context) => login()),
+      },
+    );
+  }
+}
